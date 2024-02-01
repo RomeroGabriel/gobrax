ec1c1729f6cb38576dfafd28770c8d1b259ce3df
RomeroGabriel <gabrielromerods@hotmail.com>
2024-02-01 11:21:36 -0300

    add first version of truck driver repository

--- internal/infra/db/truck_driver_repository.go
+++ internal/infra/db/truck_driver_repository.go
@@ -32,8 +32,8 @@ func NewTruckDriverRepository(db *sqlx.DB) *TruckDriverRepository {
 	return &TruckDriverRepository{db}
 }
 
-func (r *TruckDriverRepository) Save(order *entity.TruckDriver) error {
-	_, err := r.Db.Exec("INSERT INTO TruckDriver (id, name, email, licensenumber) VALUES (?, ?, ?, ?)", order.ID.String(), order.Name, order.Email, order.LicenseNumber)
+func (r *TruckDriverRepository) Save(truckDriver *entity.TruckDriver) error {
+	_, err := r.Db.Exec("INSERT INTO TruckDriver (id, name, email, licensenumber) VALUES (?, ?, ?, ?)", truckDriver.ID.String(), truckDriver.Name, truckDriver.Email, truckDriver.LicenseNumber)
 	if err != nil {
 		return err
 	}
@@ -43,8 +43,35 @@ func (r *TruckDriverRepository) Save(order *entity.TruckDriver) error {
 func (r *TruckDriverRepository) FindById(id string) (*entity.TruckDriver, error) {
 	tDriver := entity.TruckDriver{}
 	err := r.Db.Get(&tDriver, "SELECT id, name, email, licensenumber FROM TruckDriver WHERE id = ?", id)
+	return &tDriver, err
+}
+
+func (r *TruckDriverRepository) FindAll() ([]entity.TruckDriver, error) {
+	tAll := []entity.TruckDriver{}
+	err := r.Db.Select(&tAll, "SELECT id, name, email, licensenumber FROM TruckDriver")
 	if err != nil {
 		return nil, err
 	}
-	return &tDriver, err
+	return tAll, err
+}
+
+func (r *TruckDriverRepository) Update(truckDriver *entity.TruckDriver) error {
+	_, err := r.Db.Exec("UPDATE TruckDriver SET name = ?, email = ?, licensenumber = ?  WHERE id = ?", truckDriver.Name, truckDriver.Email, truckDriver.LicenseNumber, truckDriver.ID.String())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *TruckDriverRepository) Delete(id string) error {
+	_, err := r.FindById(id)
+	if err != nil {
+		return err
+	}
+	// TODO: Add logical delete
+	_, err = r.Db.Exec("DELETE FROM TruckDriver WHERE id = ?", id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- internal/infra/db/truck_driver_repository_test.go
+++ internal/infra/db/truck_driver_repository_test.go
@@ -4,6 +4,7 @@
 package db
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/RomeroGabriel/gobrax-challenge/internal/entity"
@@ -12,29 +13,29 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-type OrderRepositoryTestSuite struct {
+type TruckDriverRepositoryTestSuite struct {
 	suite.Suite
 	Db *sqlx.DB
 }
 
-func (suite *OrderRepositoryTestSuite) SetupSuite() {
+func (suite *TruckDriverRepositoryTestSuite) SetupSuite() {
 	db := sqlx.MustOpen("sqlite3", ":memory:")
 	suite.Db = db
 }
 
-func (suite *OrderRepositoryTestSuite) TearDownSuite() {
+func (suite *TruckDriverRepositoryTestSuite) TearDownSuite() {
 	suite.Db.Close()
 }
 
 func TestSuite(t *testing.T) {
-	suite.Run(t, new(OrderRepositoryTestSuite))
+	suite.Run(t, new(TruckDriverRepositoryTestSuite))
 }
 
 var name = "Trucker 1"
 var email = "email1@gg.com"
 var license = "ABC12345"
 
-func (suite *OrderRepositoryTestSuite) TestSaveTruckDriver() {
+func (suite *TruckDriverRepositoryTestSuite) TestSaveTruckDriver() {
 	tDriver, err := entity.NewTruckDriver(name, email, license)
 	suite.NoError(err)
 	repo := NewTruckDriverRepository(suite.Db)
@@ -42,7 +43,18 @@ func (suite *OrderRepositoryTestSuite) TestSaveTruckDriver() {
 	suite.NoError(err)
 }
 
-func (suite *OrderRepositoryTestSuite) TestFindByIdTruckDriver() {
+func (suite *TruckDriverRepositoryTestSuite) TestSaveDuplicateTruckDriver() {
+	tDriver, err := entity.NewTruckDriver(name, email, license)
+	suite.NoError(err)
+	repo := NewTruckDriverRepository(suite.Db)
+	err = repo.Save(tDriver)
+	suite.NoError(err)
+
+	err = repo.Save(tDriver)
+	suite.Error(err)
+}
+
+func (suite *TruckDriverRepositoryTestSuite) TestFindByIdTruckDriver() {
 	tDriver, err := entity.NewTruckDriver(name, email, license)
 	suite.NoError(err)
 	repo := NewTruckDriverRepository(suite.Db)
@@ -54,4 +66,70 @@ func (suite *OrderRepositoryTestSuite) TestFindByIdTruckDriver() {
 	suite.Equal(name, tDriverFind.Name)
 	suite.Equal(email, tDriverFind.Email)
 	suite.Equal(license, tDriverFind.LicenseNumber)
+}
+
+func (suite *TruckDriverRepositoryTestSuite) TestFindByIdTruckDriverNotExist() {
+	tDriver, err := entity.NewTruckDriver(name, email, license)
+	suite.NoError(err)
+	repo := NewTruckDriverRepository(suite.Db)
+
+	tDriverFind, err := repo.FindById(tDriver.ID.String())
+	suite.Error(err)
+	suite.Equal("", tDriverFind.Name)
+	suite.Equal("", tDriverFind.Email)
+	suite.Equal("", tDriverFind.LicenseNumber)
+}
+
+func (suite *TruckDriverRepositoryTestSuite) TestFindAllTruckDriver() {
+	repo := NewTruckDriverRepository(suite.Db)
+
+	for i := 0; i < 10; i++ {
+		td, err := entity.NewTruckDriver(fmt.Sprintf("TruckDriver %d", i), email, license)
+		suite.NoError(err)
+		err = repo.Save(td)
+		suite.NoError(err)
+	}
+
+	tds, err := repo.FindAll()
+	suite.NoError(err)
+	suite.Len(tds, 10)
+	suite.Equal("TruckDriver 0", tds[0].Name)
+	suite.Equal("TruckDriver 9", tds[9].Name)
+}
+
+func (suite *TruckDriverRepositoryTestSuite) TestUpdateTruckDriver() {
+	repo := NewTruckDriverRepository(suite.Db)
+
+	tDriver, err := entity.NewTruckDriver(name, email, license)
+	suite.NoError(err)
+	err = repo.Save(tDriver)
+	suite.NoError(err)
+
+	tDriver.Name = "Update Name"
+	tDriver.Email = "Update email"
+	tDriver.LicenseNumber = "Update License"
+	err = repo.Update(tDriver)
+
+	tDriverFind, err := repo.FindById(tDriver.ID.String())
+	suite.NoError(err)
+	suite.Equal("Update Name", tDriverFind.Name)
+	suite.Equal("Update email", tDriverFind.Email)
+	suite.Equal("Update License", tDriverFind.LicenseNumber)
+}
+
+func (suite *TruckDriverRepositoryTestSuite) TestDeleteTruckDriver() {
+	repo := NewTruckDriverRepository(suite.Db)
+	tDriver, err := entity.NewTruckDriver(name, email, license)
+
+	suite.NoError(err)
+	err = repo.Save(tDriver)
+	suite.NoError(err)
+
+	err = repo.Delete(tDriver.ID.String())
+
+	tDriverFind, err := repo.FindById(tDriver.ID.String())
+	suite.Error(err)
+	suite.Equal("", tDriverFind.Name)
+	suite.Equal("", tDriverFind.Email)
+	suite.Equal("", tDriverFind.LicenseNumber)
 }
